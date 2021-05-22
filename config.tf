c6398e0085fd43c984748cde3fcf7c5f1be62b43
conmurphy <conormurphy33@hotmail.com>
2021-05-22 21:30:20 +0200

    Update config.tf

--- config.tf
+++ config.tf
@@ -18,12 +18,12 @@ resource "aci_subnet" "demosubnet" {
 
 resource "aci_application_profile" "myWebsite" {
   tenant_dn = "${aci_tenant.conmurph_intro_to_terraform.id}"
-  name       = "my_website-ap"
+  name       = "my_website"
 }
 
 resource "aci_application_epg" "web" {
     application_profile_dn  = "${aci_application_profile.myWebsite.id}"
-    name                            = "web-epg"
+    name                            = "web"
     description                   = "this is the web epg created by terraform"
     flood_on_encap            = "disabled"
     fwd_ctrl                    = "none"
