d15e0be53ad8630f8ec0b966343333d1e33d5077
conmurphy <conormurphy33@hotmail.com>
2021-05-25 15:57:24 +0200

    Update config.tf

--- config.tf
+++ config.tf
@@ -36,6 +36,21 @@ resource "aci_application_epg" "web" {
     shutdown                    = "no"
   }
 
+resource "aci_application_epg" "app" {
+    application_profile_dn  = "${aci_application_profile.myWebsite.id}"
+    name                            = "app"
+    description                   = "this is the app epg created by terraform"
+    flood_on_encap            = "disabled"
+    fwd_ctrl                    = "none"
+    has_mcast_source            = "no"
+    match_t                         = "AtleastOne"
+    name_alias                  = "app"
+    pc_enf_pref                 = "unenforced"
+    pref_gr_memb                = "exclude"
+    prio                            = "unspecified"
+    shutdown                    = "no"
+  }
+
 
 resource "aci_application_epg" "db" {
     application_profile_dn  = "${aci_application_profile.myWebsite.id}"
