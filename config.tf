e3db5cc0b4df37d0616d843f0ed6746cfa727d79
conmurphy <conormurphy33@hotmail.com>
2021-09-16 11:33:23 +0200

    Update config.tf

--- config.tf
+++ config.tf
@@ -2,12 +2,13 @@
 resource "aci_tenant" "conmurph_intro_to_terraform" {
   name        = "conmurph_intro_to_terraform"    
 }
-/*
+
 resource "aci_bridge_domain" "bd_for_subnet" {
   tenant_dn   = "${aci_tenant.conmurph_intro_to_terraform.id}"
   name        = "bd_for_subnet"
   description = "This bridge domain is created by the Terraform ACI provider"
 }
+/*
 
 resource "aci_subnet" "demosubnet" {
   parent_dn                    = "${aci_bridge_domain.bd_for_subnet.id}"
